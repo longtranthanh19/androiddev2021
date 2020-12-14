ef15687b877c045a45e1a9608b0266664666c9c8
Tran Thanh Long <longtranthanh6@gmail.com>
2020-12-14 22:37:42 +0700

    Reset Basic Activity

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastActivity.java
@@ -0,0 +1,22 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle SaveInstanceState) {
+        View forecastFragment = inflater.inflate(R.layout.activity_forecast, container, false);
+        forecastFragment.setBackgroundColor(Color.parseColor("#BEC4F1"));
+
+        return forecastFragment;
+    }
+}
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,5 +1,7 @@
 package vn.edu.usth.weather;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.util.Log;
 
+
 public class WeatherActivity extends AppCompatActivity {
     private final String tag = "status";
 
@@ -18,53 +21,41 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.container, firstFragment).commit();
+        Log.i(tag, "on create...");
+
+        ForecastActivity firstFragment = new ForecastActivity();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
     }
 
-        @Override
-        protected void onStart() {
-            Log.i(tag, "on start");
-            super.onStart();
-        }
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-            Log.i(tag, "on resume");
-        }
-
-        @Override
-        protected void onPause() {
-            super.onPause();
-            Log.i(tag, "on pause");
-        }
-
-        @Override
-        protected void onStop() {
-            super.onStop();
-            Log.i(tag, "on stop");
-        }
-
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            Log.i(tag, "on destroy");
-        }
-
+    @Override
+    protected void onStart() {
+        Log.i(tag, "on start");
+        super.onStart();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(tag, "on resume");
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(tag, "on pause");
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag, "on stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(tag, "on destroy");
+    }
+
+}
