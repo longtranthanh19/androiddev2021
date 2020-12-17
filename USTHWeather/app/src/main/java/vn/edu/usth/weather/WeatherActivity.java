54fb6e39b725b93c4c39fb85b71f5055c246e2a4
Tran Thanh Long <longtranthanh6@gmail.com>
2020-12-17 09:58:53 +0700

    Update Labwork 7

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,6 +3,14 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.util.Log;
 
@@ -17,8 +25,14 @@ public class WeatherActivity extends AppCompatActivity {
 
         Log.i(tag, "on create...");
 
-        ForecastActivity firstFragment = new ForecastActivity();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+//        ForecastActivity firstFragment = new ForecastActivity();
+//        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
 
     @Override
@@ -51,4 +65,28 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(tag, "on destroy");
     }
 
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0:
+                    return new WeatherAndForecastFragment();
+                case 1:
+                    return new WeatherAndForecastFragment();
+                case 2:
+                    return new WeatherAndForecastFragment();
+            }
+            return new Fragment();
+        }
+    }
 }
