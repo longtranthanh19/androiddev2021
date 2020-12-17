ec895569cced02fc4d8e862c33555597ab74cf8d
Tran Thanh Long <longtranthanh6@gmail.com>
2020-12-17 23:21:38 +0700

    Update Labwork 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -8,18 +10,21 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 import android.util.Log;
 
-import com.google.android.material.tabs.TabLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class WeatherActivity extends AppCompatActivity {
     private final String tag = "status";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,6 @@ public class WeatherActivity extends AppCompatActivity {
 
         Log.i(tag, "on create...");
 
-//        ForecastActivity firstFragment = new ForecastActivity();
-//        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
-
         PagerAdapter adapter = new HomeFragmentPagerAdapter(
                 getSupportFragmentManager());
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
@@ -38,18 +40,22 @@ public class WeatherActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
+
+        MediaPlayer player = MediaPlayer.create(this, R.raw.song);
+        player.start();
     }
 
     @Override
     protected void onStart() {
         Log.i(tag, "on start");
         super.onStart();
+        copyFileToExternalStorage(R.raw.song, "song.mp3");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(tag, "on resume");
+        Log.i(tag, "on resume");;
     }
 
     @Override
@@ -98,6 +104,34 @@ public class WeatherActivity extends AppCompatActivity {
         @Override
         public CharSequence getPageTitle(int page){
             return titles[page];
+        }
+    }
+
+    private void copyFileToExternalStorage(int resourceId, String resourceName) {
+        try {
+            File file = new File(getExternalFilesDir(null), resourceName);
+            InputStream in = getApplicationContext().getResources().openRawResource(resourceId);
+            OutputStream out = new FileOutputStream(file);
+            byte[] buff = new byte[1024 * 2];
+            int read = 0;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            }
+            finally {
+                Toast toast = Toast.makeText(getApplicationContext(), file.getAbsolutePath(), Toast.LENGTH_LONG);
+                toast.show();
+                in.close();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast toast = Toast.makeText(getApplicationContext(), "File not found", Toast.LENGTH_LONG);
+            toast.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
